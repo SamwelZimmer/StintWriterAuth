e8e9dcda347e89187aae2d04e47d1cc4ccd1cf86
Sam <sampharris2000@hotmail.co.uk>
2023-10-15 23:40:40 +0100

    Change Redirect Address

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const DEVELOPMENT_APP_SERVER = "http://localhost:3001/";
 export const DEVELOPMENT_AUTH_SERVER = "http://localhost:3000/";
 
-export const PRODUCTION_APP_SERVER = "http://localhost:3001/";
+export const PRODUCTION_APP_SERVER = "https://stint-writer-auth.vercel.app/";
 export const PRODUCTION_AUTH_SERVER = "http://localhost:3000/";
 
 export const CURRENT_AUTH_SERVER = DEVELOPMENT_AUTH_SERVER;
